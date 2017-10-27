4a0c9996ede4f5b94dca884c88c26ea4ba8c47ba
JesseHarder <jhharder@scu.edu>
2017-10-27 15:04:33 -0700

    Added function for rolling more than 1 die.

--- main.py
+++ main.py
@@ -41,7 +41,26 @@ def roll_exploding_die(num_sides=DEFAULT_DIE_SIZE, success_threshold=DEFAULT_SUC
     return successes
 
 
+# Function: roll_exploding_dice
+# Parameters:
+#   num_dice- expected to be an integer > 0.
+#       Default value is 1
+#   num_sides - expected to be an integer > 0.
+#       Default value is DEFAULT_DIE_SIZE
+#   num_sides - expected to be an integer > 0.
+#       Default value is DEFAULT_SUCCESS_THRESHOLD
+# Return Value: The number of successes obtained from a simulation of rolling an num_dice exploding dice with
+#               num_sides sides and when a success is any roll of success_threshold or higher.
+def roll_exploding_dice(num_dice=1, num_sides=DEFAULT_DIE_SIZE, success_threshold=DEFAULT_SUCCESS_THRESHOLD):
+    total_successess = 0
+
+    # Roll num_dice and count successes.
+    for i in range(1,num_dice):
+        total_successess += roll_exploding_die(num_sides, success_threshold)
+
+    return total_successess
+
 if __name__ == "__main__":
     print('Program running')
-    for i in range(1, 100):
-        print("Rolled %s successes" % roll_exploding_die())
+    for i in range(1, 10):
+        print("Rolled %s successes" % roll_exploding_dice(3))
